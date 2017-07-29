9ce2407a728aaa2a874892a147501898639062c7
edgomez18 <146097@p.uapa.edu.do>
2017-07-28 21:55:42 -0400

    Se a;adio un Home, para manejar las ventanas por Menus.

---
+++ Crud/Home.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Crud
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.empleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verEmpleadosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nominaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignarNominaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verNominasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.empleadosToolStripMenuItem,
+            this.nominaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // empleadosToolStripMenuItem
+            // 
+            this.empleadosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verEmpleadosToolStripMenuItem,
+            this.administrarToolStripMenuItem});
+            this.empleadosToolStripMenuItem.Name = "empleadosToolStripMenuItem";
+            this.empleadosToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.empleadosToolStripMenuItem.Text = "Empleados";
+            // 
+            // verEmpleadosToolStripMenuItem
+            // 
+            this.verEmpleadosToolStripMenuItem.Name = "verEmpleadosToolStripMenuItem";
+            this.verEmpleadosToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.verEmpleadosToolStripMenuItem.Text = "Agregar";
+            this.verEmpleadosToolStripMenuItem.Click += new System.EventHandler(this.verEmpleadosToolStripMenuItem_Click);
+            // 
+            // administrarToolStripMenuItem
+            // 
+            this.administrarToolStripMenuItem.Name = "administrarToolStripMenuItem";
+            this.administrarToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.administrarToolStripMenuItem.Text = "Administrar";
+            this.administrarToolStripMenuItem.Click += new System.EventHandler(this.administrarToolStripMenuItem_Click);
+            // 
+            // nominaToolStripMenuItem
+            // 
+            this.nominaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.asignarNominaToolStripMenuItem,
+            this.verNominasToolStripMenuItem});
+            this.nominaToolStripMenuItem.Name = "nominaToolStripMenuItem";
+            this.nominaToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.nominaToolStripMenuItem.Text = "Nomina";
+            // 
+            // asignarNominaToolStripMenuItem
+            // 
+            this.asignarNominaToolStripMenuItem.Name = "asignarNominaToolStripMenuItem";
+            this.asignarNominaToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.asignarNominaToolStripMenuItem.Text = "Asignar nomina";
+            this.asignarNominaToolStripMenuItem.Click += new System.EventHandler(this.asignarNominaToolStripMenuItem_Click);
+            // 
+            // verNominasToolStripMenuItem
+            // 
+            this.verNominasToolStripMenuItem.Name = "verNominasToolStripMenuItem";
+            this.verNominasToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.verNominasToolStripMenuItem.Text = "Ver nominas ";
+            this.verNominasToolStripMenuItem.Click += new System.EventHandler(this.verNominasToolStripMenuItem_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Home";
+            this.Text = "Home";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem empleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verEmpleadosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nominaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignarNominaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verNominasToolStripMenuItem;
+    }
+}
---
+++ Crud/Home.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void verEmpleadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CEmpleado frm = new CEmpleado();
+            frm.Show(); 
+        }
+
+        private void administrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            manager frm = new manager();
+            frm.Show();
+        }
+
+        private void asignarNominaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            addrouster frm = new addrouster();
+            frm.Show();
+        }
+
+        private void verNominasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            seeroster frm = new seeroster();
+            frm.Show();
+        }
+    }
+}
